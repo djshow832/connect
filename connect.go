95ee54e47fcc234caf3329d3ecc5e0152283e466
djshow832 <zhangming@pingcap.com>
2024-09-19 20:07:19 +0800

    add args

--- connect.go
+++ connect.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 
@@ -14,40 +15,50 @@ import (
 
 func main() {
 	path := os.Args[1]
+	conns, err := strconv.ParseInt(os.Args[2], 10, 64)
+	if err != nil {
+		panic(err)
+	}
 	db, err := sql.Open("mysql", path)
 	if err != nil {
 		panic(errors.Wrap(err, "open db fails"))
 	}
 	defer db.Close()
 	var wg sync.WaitGroup
-	wg.Add(2)
+	wg.Add(int(conns) + 1)
 
 	// long connnection
-	go func() {
-		defer wg.Done()
-		ticker := time.NewTicker(time.Second)
-		defer ticker.Stop()
-		for {
-			conn, err := db.Conn(context.Background())
-			if err != nil {
-				fmt.Println("create connection fails", time.Now(), err)
-				<-ticker.C
-				continue
-			}
-
+	for i := 0; i < int(conns); i++ {
+		go func() {
+			defer wg.Done()
+			ticker := time.NewTicker(time.Second)
+			defer ticker.Stop()
 			for {
-				err = conn.PingContext(context.Background())
+				conn, err := db.Conn(context.Background())
 				if err != nil {
-					fmt.Println("long connection fail", time.Now(), err)
+					fmt.Println("create connection fails", time.Now(), err)
 					<-ticker.C
-					break
+					continue
 				}
-				fmt.Println("long connection succeeds", time.Now())
-				<-ticker.C
+
+				for {
+					startTime := time.Now()
+					err = conn.PingContext(context.Background())
+					if err != nil {
+						fmt.Println("long connection fail", time.Now(), err)
+						<-ticker.C
+						break
+					}
+					duration := time.Since(startTime)
+					if duration > 100*time.Millisecond {
+						fmt.Println("long connection too slow", time.Now(), duration)
+					}
+					<-ticker.C
+				}
+				_ = conn.Close()
 			}
-			_ = conn.Close()
-		}
-	}()
+		}()
+	}
 
 	// short connection
 	go func() {
@@ -55,13 +66,17 @@ func main() {
 		ticker := time.NewTicker(time.Second)
 		defer ticker.Stop()
 		for {
+			startTime := time.Now()
 			err := db.Ping()
 			if err != nil {
-				fmt.Println("new connection fails", time.Now(), err)
+				fmt.Println("short connection fails", time.Now(), err)
 				<-ticker.C
 				continue
 			}
-			fmt.Println("new connection succeeds", time.Now())
+			duration := time.Since(startTime)
+			if duration > 100*time.Millisecond {
+				fmt.Println("short connection too slow", time.Now(), duration)
+			}
 			<-ticker.C
 		}
 	}()
