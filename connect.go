5350e824a602ddc7ee145d02711157436d47a038
djshow832 <zhangming@pingcap.com>
2024-09-19 17:49:54 +0800

    update

--- connect.go
+++ connect.go
@@ -1,33 +1,69 @@
 package main
 
 import (
-        "database/sql"
-        "fmt"
-        "os/exec"
-        "time"
+	"context"
+	"database/sql"
+	"fmt"
+	"os"
+	"sync"
+	"time"
 
-        _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/pkg/errors"
 )
 
 func main() {
-        path := "root:@tcp(10.2.12.200:6661)/test"
-        db, err := sql.Open("mysql", path)
-        if err != nil {
-                panic(err)
-        }
-        for {
-                start := time.Now()
-                output, err1 := exec.Command("/usr/sbin/arp", "10.2.12.200").CombinedOutput()
-                if err1 != nil {
-                        fmt.Println(err1)
-                }
-                err = db.Ping()
-                end := time.Now()
-                var dig string
-                if err != nil {
-                        dig = string(output)
-                }
-                fmt.Println(end, end.Sub(start), err, dig)
-                <-time.After(500 * time.Millisecond)
-        }
+	path := os.Args[1]
+	db, err := sql.Open("mysql", path)
+	if err != nil {
+		panic(errors.Wrap(err, "open db fails"))
+	}
+	defer db.Close()
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	// long connnection
+	go func() {
+		defer wg.Done()
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
+		for {
+			conn, err := db.Conn(context.Background())
+			if err != nil {
+				fmt.Println("create connection fails", time.Now(), err)
+				<-ticker.C
+				continue
+			}
+
+			for {
+				err = conn.PingContext(context.Background())
+				if err != nil {
+					fmt.Println("long connection fail", time.Now(), err)
+					<-ticker.C
+					break
+				}
+				fmt.Println("long connection succeeds", time.Now())
+				<-ticker.C
+			}
+			_ = conn.Close()
+		}
+	}()
+
+	// short connection
+	go func() {
+		defer wg.Done()
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
+		for {
+			err := db.Ping()
+			if err != nil {
+				fmt.Println("new connection fails", time.Now(), err)
+				<-ticker.C
+				continue
+			}
+			fmt.Println("new connection succeeds", time.Now())
+			<-ticker.C
+		}
+	}()
+	wg.Wait()
 }
